06891f1ef635ca9c7d6e15d4cd91bcad9b0fceb3
Gerox1811 <a3520110683@alumno.uttehuacan.edu.mx>
2023-08-23 09:11:28 -0600

    masversiones

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -13,12 +13,6 @@ export class DatabaseService {
     this.ritmoCardiacoRef = this.db.object('ritmo_cardiaco');
   }
 
-  getPulsacionesPorMinuto(): Observable<number> {
-    console.log('Fetching pulsaciones por minuto from Firebase Realtime Database...');
-    return this.ritmoCardiacoRef.valueChanges().pipe(
-      map(pulsaciones => pulsaciones || 0) // Filtrar para eliminar los valores nulos
-    );
-  }
   
   getDatosFueraDeRango(): Observable<any[]> {
     const datosFueraDeRangoRef = this.db.list('datosFueraDeRango');
